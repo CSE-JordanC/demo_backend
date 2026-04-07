795b9d5baeb078cc87d7bc5724794c765b6ca7cb
CSE-JordanC <chavousn@email.sc.edu>
2026-04-07 09:15:02 -0400

    script fixed

--- app.js
+++ app.js
@@ -7,91 +7,91 @@ app.use(express.json());
 app.use(cors());
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, "./public/images/");
-    },
-    filename: (req, file, cb) => {
-      cb(null, file.originalname);
-    },
-  });
-  
+  destination: (req, file, cb) => {
+    cb(null, "./public/images/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
 const upload = multer({ storage: storage });
 
 let houses = [
-    {
-        "_id":1,
-        "name": "Farmhouse",
-        "size": 2000,
-        "bedrooms": 3,
-        "bathrooms": 2.5,
-        "features": [
-            "wrap around porch",
-            "attached garage"
-        ],
-        "main_image": "farm.webp",
-        "floor_plans": [
-            {
-                "name": "Main Level",
-                "image": "farm-floor1.webp"
-            },
-            {
-                "name": "Basement",
-                "image": "farm-floor2.webp"
-            }
-        ]
-    },
-    {
-        "_id":2,
-        "name": "Mountain House",
-        "size": 1700,
-        "bedrooms": 3,
-        "bathrooms": 2,
-        "features": [
-            "grand porch",
-            "covered deck"
-        ],
-        "main_image": "mountain-house.webp",
-        "floor_plans": [
-            {
-                "name": "Main Level",
-                "image": "mountain-house1.webp"
-            },
-            {
-                "name": "Optional Lower Level",
-                "image": "mountain-house2.webp"
-            },
-            {
-                "name": "Main Level Slab Option",
-                "image": "mountain-house3.jpg"
-            }
-        ]
-    },
-    {
-        "_id":3,
-        "name": "Lake House",
-        "size": 3000,
-        "bedrooms": 4,
-        "bathrooms": 3,
-        "features": [
-            "covered deck",
-            "outdoor kitchen",
-            "pool house"
-        ],
-        "main_image": "lake-house.jpg",
-        "floor_plans": [
-            {
-                "name": "Main Level",
-                "image": "lake-house1.webp"
-            },
-            {
-                "name": "Lower Level",
-                "image": "lake-house2.webp"
-            }
-        ]
-    }
+  {
+    "_id":1,
+    "name": "Farmhouse",
+    "size": 2000,
+    "bedrooms": 3,
+    "bathrooms": 2.5,
+    "features": [
+        "wrap around porch",
+        "attached garage"
+    ],
+    "main_image": "farm.webp",
+    "floor_plans": [
+        {
+            "name": "Main Level",
+            "image": "farm-floor1.webp"
+        },
+        {
+            "name": "Basement",
+            "image": "farm-floor2.webp"
+        }
+    ]
+  },
+  {
+      "_id":2,
+      "name": "Mountain House",
+      "size": 1700,
+      "bedrooms": 3,
+      "bathrooms": 2,
+      "features": [
+          "grand porch",
+          "covered deck"
+      ],
+      "main_image": "mountain-house.webp",
+      "floor_plans": [
+          {
+              "name": "Main Level",
+              "image": "mountain-house1.webp"
+          },
+          {
+              "name": "Optional Lower Level",
+              "image": "mountain-house2.webp"
+          },
+          {
+              "name": "Main Level Slab Option",
+              "image": "mountain-house3.jpg"
+          }
+      ]
+  },
+  {
+      "_id":3,
+      "name": "Lake House",
+      "size": 3000,
+      "bedrooms": 4,
+      "bathrooms": 3,
+      "features": [
+          "covered deck",
+          "outdoor kitchen",
+          "pool house"
+      ],
+      "main_image": "lake-house.jpg",
+      "floor_plans": [
+          {
+              "name": "Main Level",
+              "image": "lake-house1.webp"
+          },
+          {
+              "name": "Lower Level",
+              "image": "lake-house2.webp"
+          }
+      ]
+  }
 ]
 
-app.get("/api/houses",(req,res)=>{
+app.get("/api/houses", (req,res)=>{
   res.send(houses);
 });
 
@@ -101,7 +101,6 @@ app.get("/api/houses/:id", (req,res)=>{
 });
 
 //listen for incoming requests
-const port = process.env.PORT || 3001;
-app.listen(port, "0.0.0.0", () => {
-  console.log(`Server is up and running on ${port}`);
+app.listen(3001, ()=>{
+  console.log("Server is up and running");
 });
